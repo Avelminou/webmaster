6d5afdc685424e67164241f99021a9c9e945fa67
Avelminou <avelmimilitia@gmail.com>
2026-03-15 15:44:53 +0300

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -5,13 +5,13 @@ let btnQ = document.getElementById("btnQ");
 
 const pro = document.getElementById('pro');
 
-const bouton = [btnD,btnT,btnQ]
+const bouton = [btnT,btnQ]
 
 bouton.forEach(function(coloBtn, i) {
     coloBtn.onclick = () => {
         btnU.style.background = 'white';
         pro.innerHTML = `
-        <iframe src="./app/off/app${i}.html" frameborder="0"></iframe>
+        <iframe src="./app/outils/app${i}.html" frameborder="0"></iframe>
         `;
 
     }
@@ -21,3 +21,17 @@ btnU.onclick = () => {
     btnU.style.background = 'rgb(221, 225, 250)';
     location.reload();
 }
+
+btnD.onclick = () =>{
+    window.location.href = './app/outils/server.html'
+}
+
+const btnLesson = document.querySelectorAll('.recent');
+
+btnLesson.forEach(function(lesson, i) {
+    lesson.onclick = () => {
+        pro.innerHTML = `
+            <iframe src="https://avelminou.github.io/webmaster/app/off/lesson${i}.html" frameborder="0"></iframe>
+            `;
+        }
+})
