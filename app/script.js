979dca3ab70b6c0ec65e6713393f496ac2cb9726
Avelminou <avelmimilitia@gmail.com>
2026-03-15 17:43:51 +0300

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -4,34 +4,94 @@ let btnT = document.getElementById("btnT");
 let btnQ = document.getElementById("btnQ");
 
 const pro = document.getElementById('pro');
+const nbL = document.getElementById('nbL');
 
-const bouton = [btnT,btnQ]
+const bouton = [btnT, btnQ]
 
-bouton.forEach(function(coloBtn, i) {
+bouton.forEach(function (coloBtn, i) {
+
     coloBtn.onclick = () => {
+
         btnU.style.background = 'white';
-        pro.innerHTML = `
-        <iframe src="./app/outils/app${i}.html" frameborder="0"></iframe>
-        `;
+
+        const url = `https://avelminou.github.io/webmaster/app/outils/app${i}.html`;
+
+        fetch(url)
+            .then(() => {
+                pro.innerHTML = `
+            <iframe src="${url}" frameborder="0"></iframe>
+            `;
+            })
+            .catch(() => {
+                pro.innerHTML = `
+            <iframe src="./app/all.html" frameborder="0"></iframe>
+            `;
+            });
 
     }
-})
 
+});
+btnU.style.background = 'rgb(221, 225, 250)';
 btnU.onclick = () => {
     btnU.style.background = 'rgb(221, 225, 250)';
     location.reload();
 }
 
-btnD.onclick = () =>{
+btnD.onclick = () => {
     window.location.href = './app/outils/server.html'
 }
 
-const btnLesson = document.querySelectorAll('.recent');
+const container = document.getElementById("histo");
 
-btnLesson.forEach(function(lesson, i) {
-    lesson.onclick = () => {
-        pro.innerHTML = `
-            <iframe src="https://avelminou.github.io/webmaster/app/off/lesson${i}.html" frameborder="0"></iframe>
-            `;
+
+const lessons = [
+    "Balises de base",
+    "Les titres HTML",
+    "Les paragraphes",
+    "Les liens",
+    "Les images"
+];
+
+lessons.forEach((title, i) => {
+
+    const recent = document.createElement("div");
+    recent.className = "recent";
+
+    recent.innerHTML = `
+<img src="./app/image/icons8-historique-100.png" width="30">
+<b>&nbsp;&nbsp;${title}</b>
+`;
+
+    recent.onclick = async () => {
+
+        const url = `https://avelminou.github.io/webmaster/app/off/lesson${i}.html`;
+
+        pro.innerHTML = "Chargement...";
+
+        try {
+
+            const res = await fetch(url);
+
+            if (!res.ok) throw new Error();
+
+            pro.innerHTML = `
+<iframe src="${url}" frameborder="0"></iframe>
+`;
+
+        } catch {
+
+            pro.innerHTML = `
+<iframe src="./app/all.html" frameborder="0"></iframe>
+`;
+
         }
-})
+
+    };
+
+    container.appendChild(recent);
+
+});
+
+const chifre = document.querySelectorAll('.recent').length;
+
+nbL.innerText = chifre;
